4270946c4ecfed79cdcf6d921e4285b87adcf16a
Thomas Soares <thomas__soares@hotmail.com>
2021-07-12 23:28:10 -0300

    Funçao de autenticaçao com Google

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
+  function signInWithGoogle() {}
+
   return (
     <BrowserRouter>
       <Route path="/" exact={true} component={Home} />
