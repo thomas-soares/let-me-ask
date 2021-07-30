0feb2692aecea1dec0b738aef712bf57e7e02e98
Thomas Soares <thomas__soares@hotmail.com>
2021-07-29 22:48:19 -0300

    Instanciado o resultado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
 export function AuthContextProvider(props) {
   const provider = new firebase.auth.GoogleAuthProvider();
 
+  const result = await auth.signInWithPopup(provider);
+
   return <AuthContext.Provider></AuthContext.Provider>;
 }
