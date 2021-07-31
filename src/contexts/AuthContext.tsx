e557b792c0fb512643b285690234ae169c071e8d
Thomas Soares <thomas__soares@hotmail.com>
2021-07-30 21:51:22 -0300

    Funçao de logar com Google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,9 @@
 export function AuthContextProvider(props) {
-  const provider = new firebase.auth.GoogleAuthProvider();
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
 
-  const result = await auth.signInWithPopup(provider);
+    const result = await auth.signInWithPopup(provider);
+  }
 
   return <AuthContext.Provider></AuthContext.Provider>;
 }
