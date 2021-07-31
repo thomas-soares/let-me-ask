4d6a77d3b02df483d0d0f90f8757bea2b8448c95
Thomas Soares <thomas__soares@hotmail.com>
2021-07-30 21:52:45 -0300

    Pegando dados do usuario

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,6 +3,10 @@ export function AuthContextProvider(props) {
     const provider = new firebase.auth.GoogleAuthProvider();
 
     const result = await auth.signInWithPopup(provider);
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+    }
   }
 
   return <AuthContext.Provider></AuthContext.Provider>;
