6158d2956bd70bd22704002cf7f6b50b42ede026
Thomas Soares <thomas__soares@hotmail.com>
2021-08-11 21:29:03 -0300

    Importando useHistory

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from "react-router-dom";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
