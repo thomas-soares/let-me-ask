d5324504ee9dd5e7f1cba935c1cd1b14b9085983
Thomas Soares <thomas__soares@hotmail.com>
2021-08-12 23:25:56 -0300

    Utilizando hook criado

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,10 +4,13 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
 import "../styles/auth.scss";
 
 export function NewRoom() {
+  const { user } = useAuth();
+
   return (
     <div id="page-auth">
       <aside>
@@ -21,6 +24,7 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Let me ask" />
+          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
           <form>
             <input type="text" placeholder="Nome da sala" />
