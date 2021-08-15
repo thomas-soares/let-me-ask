eb97c3339c0065fe3f44a02e0083fab59b9bf7ab
Thomas Soares <thomas__soares@hotmail.com>
2021-08-15 15:48:02 -0300

    Removendo texto

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,6 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Let me ask" />
-          <h2>Criar uma nova sala</h2>
           <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
