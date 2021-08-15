2ce934ca07a31d988782d83b3df8c27008544ffa
Thomas Soares <thomas__soares@hotmail.com>
2021-08-15 15:16:20 -0300

    Colocando rota de Room

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
 
@@ -11,6 +12,7 @@ function App() {
       <AuthContextProvider>
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
       </AuthContextProvider>
     </BrowserRouter>
   );
