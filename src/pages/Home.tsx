fa090c1136aa43a01d0cde80d24061a38736a841
Thomas Soares <thomas__soares@hotmail.com>
2021-08-15 16:02:07 -0300

    Validando se a sala existe

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,6 +33,11 @@ export function Home() {
     }
 
     const roomRef = await database.ref(`romms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
   }
 
   return (
