a1e9cf1d6c3df20d32063014c0431371e9ae8bb2
Thomas Soares <thomas__soares@hotmail.com>
2021-08-20 22:01:18 -0300

    Marcaçao do componente que exibe o codigo da sala

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,9 +1,12 @@
+import copyImg from "../assets/images/copy.svg";
+
 export function RoomCode() {
   return (
     <button className="room-code">
       <div>
-        <img src="" alt="" />
+        <img src={copyImg} alt="Copy room code" />
       </div>
+      <span>Sala #32765893698</span>
     </button>
   );
 }
