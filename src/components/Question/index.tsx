1145d838e49bad384fe036ca5e7d0b55e5719a3a
Thomas Soares <thomas__soares@hotmail.com>
2021-08-26 22:38:12 -0300

    Tipando as props da pergunta

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,12 @@
-export function Question() {
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+};
+
+export function Question({ content, author }: QuestionProps) {
   return (
     <div className="question">
       <p></p>
