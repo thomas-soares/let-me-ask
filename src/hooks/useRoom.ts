2bc96dca4bd8c4308f5779e1f4a5a0422964f9f1
Thomas Soares <thomas__soares@hotmail.com>
2021-08-28 23:03:18 -0300

    Passando tipagem do firebase pro hook

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,20 @@
 import { useEffect, useState } from "react";
 
+import { database } from "../services/firebase";
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
 type QuestionType = {
   id: string;
   author: {
