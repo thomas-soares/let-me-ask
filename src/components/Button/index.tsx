c0bf25837e5644b8357eb2bdbbc1dedd8110e6f0
Thomas Soares <thomas__soares@hotmail.com>
2021-09-02 20:58:45 -0300

    adicionando tipo da prop do componente de botao

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,9 @@ import { ButtonHTMLAttributes } from "react";
 
 import "./styles.scss";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+};
 
 export function Button(props: ButtonProps) {
   return <button className="button" {...props} />;
