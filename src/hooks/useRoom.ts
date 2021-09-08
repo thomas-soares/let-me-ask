38a1a8fbbc6f83d69a07932a5b6ca743f062208d
Thomas Soares <thomas__soares@hotmail.com>
2021-09-08 20:50:29 -0300

    Mudando o metodo do firebase

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -41,7 +41,7 @@ export function useRoom(roomId: string) {
   useEffect(() => {
     const roomRef = database.ref(`rooms/${roomId}`);
 
-    roomRef.once("value", (room) => {
+    roomRef.on("value", (room) => {
       const databaseRoom = room.val();
       const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
